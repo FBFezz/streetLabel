5b21642cca9f8e432f33d4d925cb020a21673c49
Fezz <43589668+FBFezz@users.noreply.github.com>
2020-06-04 19:37:52 +0100

    Update and rename __resource.lua to fxmanifest.lua

---
+++ streetLabel/fxmanifest.lua
@@ -0,0 +1,13 @@
+-- Resource Metadata
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'lowheartrate'
+description 'Street Label byLowheartrate'
+version '1.0.0'
+
+-- Client Scripts
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
